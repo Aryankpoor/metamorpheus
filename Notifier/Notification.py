850995051ed805133c7f3da3f0efd314bf7bcd18
Aryan Kapoor <64773763+Aryankpoor@users.noreply.github.com>
2020-10-07 19:32:54 +0530

    Create Notification.py

---
+++ Notifier/Notification.py
@@ -0,0 +1,17 @@
+from pynotifier import Notification
+
+Notification (
+
+    title = "Instagram",
+    
+    description = "You have 18 unread messages",
+
+    icon_path = r"C:\Users\sheetal kapoor\Desktop\Common Path\Git\favicon.ico",
+
+    duration = 25,
+
+    urgency = Notification.URGENCY_CRITICAL
+
+).send()
+
+# Hope you like this small app
