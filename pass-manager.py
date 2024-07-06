1f55842620a98e56f0d7e6396637e9cc1e1c36c5
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-06 19:49:27 +0530

    added key

--- pass-manager.py
+++ pass-manager.py
@@ -1,11 +1,30 @@
+from cryptography.fernet import Fernet
+
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', "wb") as key_file:
+        key_file.write(key)
+        
+
+def load_key():
+    file = open('key.key', "rb").read()
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
 master_pwd = input("What is the master password")
+
+
+
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = print(line.rstrip())
             user, passw = data.split("|")
-            
+            print("User:", user, ",Password:", passw)
 
 def add():
     name = input("Account name: ")
