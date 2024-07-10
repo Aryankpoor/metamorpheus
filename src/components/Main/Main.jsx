808e1f315733252efe6bd59b9ff79f0aa5dd459b
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-10 07:24:15 +0530

    added ui for main page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -32,6 +32,18 @@ const Main = () => {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder="Enter a prompt here" />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">Core may display inaccurate info, including about people, so double-check it's responses</p>
+            </div>
         </div>
     </div>
   )
