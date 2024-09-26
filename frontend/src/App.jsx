7c78fef46c54e5f6079a0ca3429f5a79c3d01964
Aryankpoor <kapooraryan0419@gmail.com>
2024-09-26 22:07:55 +0530

    added appwrite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
+import { Vite } from 'appwrite';
 
 const App = () => {
   return (
@@ -10,5 +11,11 @@ const App = () => {
     </>
   )
 }
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('66f5896b0009f35b3a5e');
 
 export default App
